1a12f038fd083db42df5cadacab88865467b87b4
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-23 15:27:13 +0000

    passing method as part of class

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 //For now for learning class based component we're gonna start with it:
 
 class App extends Component {
+
+  foo = () => 'Bar';
   render() {
 
     //to see how does it look like without JSX with using straight javascript:
@@ -11,11 +13,11 @@ class App extends Component {
 
     const name = 'Mahsa';
 
-    const foo = () => 'Bar';
+    // const foo = () => 'Bar';
     return (
       <div className="App">
         <h1>Hello {name.toUpperCase()}</h1>
-        <h1>Hello {foo()}</h1>
+        <h1>Hello {this.foo()}</h1>
       </div>
     );
   }
