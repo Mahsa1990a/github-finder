41eadc7b9c1c4d9aacc86fa646a7ca5fc88f98f3
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-23 17:52:51 +0000

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+
 import './App.css';
 
 //For now for learning class based component we're gonna start with it:
@@ -10,9 +12,9 @@ class App extends Component {
     //to see how does it look like without JSX with using straight javascript:
     //return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hello From React'));
 
-    const name = 'Mahsa';
-    const loading = false;
-    const showName = true;
+    // const name = 'Mahsa';
+    // const loading = false;
+    // const showName = true;
 
     // if(loading) {
     //   return <h4>Loading...</h4>
@@ -20,7 +22,9 @@ class App extends Component {
 
     return (
       <div className="App">
-        { loading ? <h4>Loading ...</h4> : <h1>Hello { showName && name }</h1>}  
+        {/* { loading ? <h4>Loading ...</h4> : <h1>Hello { showName && name }</h1>}   */}
+
+        <Navbar />
       </div>
     );
   }
