fd0ae4ec072fafecfeb1188b898e57f548a2e0a2
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-23 21:41:49 +0000

    defined state

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
+
+  constructor() {
+    super();
+    // console.log(123);
+
+    //state is javascript obj
+    this.state = {
+      id: 'id',
+      login: 'mojombo',
+      avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+      html_url: 'https://github.com/mojombo'
+    }
+  }
   render() {
     return (
       <div>
