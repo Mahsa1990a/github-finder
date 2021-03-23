145048f481c9376835d4babf3b430278dffad3b6
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-23 15:04:45 +0000

    changed functional component into class based component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello From React</h1>
-    </div>
-  );
+//For now for learning class based component we're gonna start with it:
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello From React</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
