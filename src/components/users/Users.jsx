10756e4c928cc070efe61d01c0e95c6cd2f59055
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-24 21:42:06 +0000

    added propTypes

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types'
 
 // class Users extends Component {
 
@@ -59,5 +60,10 @@ const userStyle = {
   gridTemplateColumns: 'repeat(3, 1fr)',
   gridGap: '1rem'
 }
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+};
 
 export default Users;
