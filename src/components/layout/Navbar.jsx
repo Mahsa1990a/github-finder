50c1722cb2c1875615921a94246f018b74e1a8e2
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-24 15:58:21 +0000

    refactored to functional component

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Navbar extends Component {
+const Navbar = (props) => {
 
   //if we don't want to pass props in App => < Navbar /> we use :
-  static defaultProps = {
-    title: 'Github Finder',
-    icon: 'fab fa-github'
-  }; 
+  // static defaultProps = {
+  //   title: 'Github Finder',
+  //   icon: 'fab fa-github'
+  // }; 
   
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
-  }
+  // static propTypes = {
+  //   title: PropTypes.string.isRequired,
+  //   icon: PropTypes.string.isRequired
+  // }
 
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <i className={this.props.icon}></i> {this.props.title}
-          </h1>
-      </nav>
-    )
-  }
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={props.icon}></i> {props.title}
+      </h1>
+    </nav>
+  )
 }
+
+Navbar.defaultProps = {
+  title: 'Github Finder',
+  icon: 'fab fa-github'
+}; 
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+};
 
 export default Navbar
