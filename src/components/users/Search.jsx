227732df6ef3055e815e63422e85d942e8274214
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-25 13:48:55 +0000

    added onChange event

--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -7,11 +7,16 @@ class Search extends Component {
     text:''  //it's gonna be empty by default
   }
 
+  //onChange event for when we type sth in the input, it's Firing off and update the state 
+  onChange = (e) => {   // e is even parameter
+    this.setState({ text: e.target.value });  //we set the setState to wathever we type in the box
+  }
+
   render() {
     return (
       <div>
         <form className='form'>
-          <input type="text" name='text' placeholder='Search Users...' value={this.state.text}/>
+          <input type="text" name='text' placeholder='Search Users...' value={this.state.text} onChange={this.onChange}/>
           <input type="submit" value='Search' className='btn btn-dark btn-block'/>
         </form>
       </div>
