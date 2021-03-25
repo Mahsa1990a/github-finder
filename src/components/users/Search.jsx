309bdb0e5e212fcbeb496810ea5ff4c5e4449488
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-25 13:42:54 +0000

    added state for input

--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 
 class Search extends Component {
+
+  //when we have form we want to attach state to the input:
+  state = {
+    text:''  //it's gonna be empty by default
+  }
+
   render() {
     return (
       <div>
         <form className='form'>
-          <input type="text" name='text' placeholder='Search Users...'/>
+          <input type="text" name='text' placeholder='Search Users...' value={this.state.text}/>
           <input type="submit" value='Search' className='btn btn-dark btn-block'/>
         </form>
       </div>
