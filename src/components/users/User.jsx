4d24f6eb91bcfcadb5cb016914c0e94accaeca75
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-26 22:36:39 +0000

    added hireble if statement

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Spinner from "../layout/Spinner";
 import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
 
 class User extends Component {
 
@@ -34,10 +35,17 @@ class User extends Component {
 
     const { loading } = this.props;
 
+    if (loading) {
+      return <Spinner />
+    }
+
     return (
-      <div>
-        {name}
-      </div>
+      <Fragment>
+        <Link to='/' className='btn btn-light'> Back To Search</Link>
+        {/* If someone is hirebale show green check */}
+        Hireable: {' '}
+        { hireable ? <i className='fas fa-check text-success' /> : <i className='fas fa-times-circle text-danger'/> }
+      </Fragment>
     )
   }
 }
