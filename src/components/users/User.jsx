dc314a14e912de4a815dcd6f308c630da37f1ab7
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-26 22:07:50 +0000

    Added componentDidMount

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 
 class User extends Component {
+
+  //It will fire off right away as soon as this component loaded:
+  componentDidMount() {
+    this.props.getUser(this.props.match.params.login)
+  }
+
   render() {
     return (
       <div>
