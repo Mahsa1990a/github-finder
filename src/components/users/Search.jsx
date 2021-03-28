79d9c93d952ff4df5d25a207417b06e258e59b89
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-28 22:38:32 +0000

    Refactoring into functional component

--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-class Search extends Component {
+// class Search extends Component { Refactoring into functional component
+const Search = ({ searchUsers, showClear, clearUsers }) => {
 
   //when we have form we want to attach state to the input:
   state = {
     text:''  //it's gonna be empty by default
   }
 
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired
-  };
-
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();  //we have it with submit form
     // console.log(this.state.text)
 
@@ -32,29 +26,33 @@ class Search extends Component {
   };
 
   //onChange event for when we type sth in the input, it's Firing off and update the state 
-  onChange = (e) => {   // e is even parameter
+  const onChange = (e) => {   // e is even parameter
     // this.setState({ text: e.target.value });  //we set the setState to wathever we type in the box
 
     //if we have text or email or pass we can use name :
     this.setState({ [e.target.name]: e.target.value }); // so now we can use this onChange event for all input if we need
   }
 
-  render() {
 
-    const { showClear, clearUsers } = this.props;
-    return (
-      <div>
-        <form onSubmit={this.onSubmit} className='form'>
-          <input type="text" name='text' placeholder='Search Users...' value={this.state.text} onChange={this.onChange}/>
-          <input type="submit" value='Search' className='btn btn-dark btn-block'/>
-        </form>
-        {/* So after we searched for user will show clear button not before */}
-        {showClear &&
-          <button className='btn btn-light btn-block' onClick={clearUsers}>Clear</button>
-        }
-      </div>
-    )
-  }
+  return (
+    <div>
+      <form onSubmit={this.onSubmit} className='form'>
+        <input type="text" name='text' placeholder='Search Users...' value={this.state.text} onChange={this.onChange}/>
+        <input type="submit" value='Search' className='btn btn-dark btn-block'/>
+      </form>
+      {/* So after we searched for user will show clear button not before */}
+      {showClear &&
+        <button className='btn btn-light btn-block' onClick={clearUsers}>Clear</button>
+      }
+    </div>
+  )
 }
+
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired
+};
 
 export default Search
