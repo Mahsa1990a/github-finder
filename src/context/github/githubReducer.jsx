0549aa8342c34f5b69c36a4d530196b0e2a157a6
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-29 19:27:54 +0000

    added SEARCH_USERS case

--- src/context/github/githubReducer.jsx
+++ src/context/github/githubReducer.jsx
@@ -3,6 +3,12 @@ import { SEARCH_USERS, SET_LOADING, CLEAR_USERS, GET_USER, GET_REPOS } from "../
 
 export default (state, action) => {
   switch(action.type) {
+    case SEARCH_USERS:
+      return {
+        ...state,
+        users: action.payload, 
+        loading: false
+      }
     case SET_LOADING:
       return {
         //state is immutable, means we can't reassign it, we have to make a copy of it and then add any additions or chenges to it
