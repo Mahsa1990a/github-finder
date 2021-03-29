3d607e0c0986cde9b028e596b7440f434415242b
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-29 18:51:05 +0000

    added types

---
+++ src/context/types.jsx
@@ -0,0 +1,9 @@
+// types are variable of strings that you can call to change your state within your reducer
+
+export const SEARCH_USERS = 'SEARCH_USERS';
+export const GET_USER = 'GET_USER';
+export const CLEAR_USERS = 'CLEAR_USERS';
+export const GET_REPOS = 'GET_REPOS';
+export const SET_LOADING = 'SET_LOADING';
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
