9ca3f78a5fe9aa434573766b97fc9521a60a1210
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-30 14:46:22 +0000

    we dont need those propTypes anymore

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
-import PropTypes from 'prop-types';
 import GithubContext from '../../context/github/githubContext';
 
 // class Users extends Component {
@@ -67,9 +66,9 @@ const userStyle = {
   gridGap: '1rem'
 }
 
-Users.propTypes = {
-  users: PropTypes.array.isRequired,
-  loading: PropTypes.bool.isRequired
-};
+// Users.propTypes = {
+//   users: PropTypes.array.isRequired,
+//   loading: PropTypes.bool.isRequired
+// };
 
 export default Users;
