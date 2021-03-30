b6c47e0bfee14f5b1ab953e06cd367bc018a31b9
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-30 14:45:18 +0000

    we dont need those propTypes anymore

--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -60,8 +60,8 @@ const Search = ({ setAlert }) => {
 
 Search.propTypes = {
   // searchUsers: PropTypes.func.isRequired,
-  clearUsers: PropTypes.func.isRequired,
-  showClear: PropTypes.bool.isRequired,
+  // clearUsers: PropTypes.func.isRequired,
+  // showClear: PropTypes.bool.isRequired,
   setAlert: PropTypes.func.isRequired
 };
 
